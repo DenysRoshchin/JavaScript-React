2d861f9f2d5780fa07e7c48894c65b6d8d58b13c
Denys <magicpsychill@icloud.com>
2022-04-05 14:39:41 +0300

    hello

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let numbersOfFilms = prompt('Numbers of muvies');
+
+let movieDB = {
+    count: numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let movieName = prompt('Name');
+let movieRait = prompt('Rait');
+
+movieDB.movies[movieName] = movieRait;
+
+
+console.log(movieName);
+console.log(movieRait);
+console.log(movieDB.movies);
+console.log(movieDB);
+
+
